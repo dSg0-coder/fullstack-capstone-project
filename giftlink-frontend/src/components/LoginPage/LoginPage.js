5fe7e882068e30e00c186620ec6680519b3326d2
dSg0-coder <dante.cardana@gmail.com>
2024-11-25 11:43:04 -0500

    LoginPage.js implementation

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,14 +1,71 @@
 import React, { useState,useEffect } from 'react';
+// Import urlConfig from `giftlink-frontend/src/config.js`
+import {urlConfig} from '../../config';
+// Import useAppContext `giftlink-frontend/context/AuthContext.js`
+import { useAppContext } from '../../context/AuthContext';
+// Import useNavigate from `react-router-dom` to handle navigation after successful registration.
+import { useNavigate } from 'react-router-dom';
 
 import './LoginPage.css';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    // Include a state for incorrect password.
+    const [incorrect, setIncorrect] = useState('');
+    // Create a local variable for `navigate`,`bearerToken` and `setIsLoggedIn`.
+    const navigate = useNavigate();
+    const bearerToken = sessionStorage.getItem('bearer-token');
+    const { setIsLoggedIn } = useAppContext();
+
+    // If the bearerToken has a value (user already logged in), navigate to MainPage
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+          navigate('/app')
+        }
+      }, [navigate])
 
     const handleLogin = async (e) => {
         e.preventDefault();
-}
+        //api call
+        const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+            // Set method
+            method: 'POST',
+            // Set headers
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
+        },
+        // Set body to send user details
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          })
+        });
+
+        // Access data coming from fetch API
+        const json = await res.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+            // Set user details
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+            // Set the user's state to log in using the `useAppContext`.
+          setIsLoggedIn(true);
+            // Navigate to the MainPage after logging in.
+          navigate('/app');
+        } else {
+            // Clear input and set an error message if the password is incorrect
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }
+
+      }
 
 
     return (
@@ -25,7 +82,7 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your email"
                                 value={email}
-                                onChange={(e) => setEmail(e.target.value)}
+                                onChange={(e) => {setEmail(e.target.value); setIncorrect("")}}
                             />
                         </div>
                         <div className="mb-4">
@@ -36,10 +93,12 @@ function LoginPage() {
                                 className="form-control"
                                 placeholder="Enter your password"
                                 value={password}
-                                onChange={(e) => setPassword(e.target.value)}
+                                onChange={(e) => {setPassword(e.target.value);setIncorrect("")}}
                             />
+
+                            {/*Display an error message to the user.*/}
+                            <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
                         </div>
-                        {/* Include appropriate error message if login is incorrect*/}
                         <button className="btn btn-primary w-100 mb-3" onClick={handleLogin}>Login</button>
                         <p className="mt-4 text-center">
                             New here? <a href="/app/register" className="text-primary">Register Here</a>
