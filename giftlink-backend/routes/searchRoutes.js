a34a8fbee12fb8cb24872d3bf026844c345f7b3f
dSg0-coder <dante.cardana@gmail.com>
2024-11-26 05:15:44 -0500

    searchRoutes.js semicolon fix

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res, next) => {
         }
 
         // Fetch filtered gifts
-        const gifts = await collection.find(query).toArray()
+        const gifts = await collection.find(query).toArray();
 
         res.json(gifts);
     } catch (e) {
